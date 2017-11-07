94b43a526eea413326b46cf171844b9a024ace29
Tirlex <32830808+Tirlex@users.noreply.github.com>
2017-11-07 12:16:35 +0200

    Delete TEMA1_PUNCTUL2_Tirlea_Razvan_421C.m

--- TEMA1_PUNCTUL2_Tirlea_Razvan_421C.m
+++
@@ -1,9 +0,0 @@
-function[]=TEMA1_PUNCTUL2_Tirlea_Razvan_421C(t,nr)
-width = 0.5;
-%sawtooth(t,width) genereaza un semnal triunghiular iar width determina
-%unde se afla maximul
-%Inmultest t cu 1/6 pentru ca panta sa devina +1 V/s
-s=(1+width)*sawtooth(t*2*1/6*pi, width) - width;
-figure(nr)
-plot(t,s,'-r.'),title('Semnal triunghiular'),xlabel('Timp [s]'),ylabel('A [V]'),grid
-end
